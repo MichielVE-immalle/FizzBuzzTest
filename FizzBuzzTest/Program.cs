1d8bffa8694cfe11a7b36abb6d00b2c93e9ab0f6
Michiel Van Eester <michiel.vaneester@immalle.be>
2016-11-21 11:18:52 +0100

    FizzBuzzTest

--- FizzBuzzTest/Program.cs
+++ FizzBuzzTest/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace FizzBuzzTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
         }
     }
 }
